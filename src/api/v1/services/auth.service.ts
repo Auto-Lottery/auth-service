8ac7925549849f9907220b117f32a3a7f134a83f
khishigee <hishigbayr_3@yahoo.com>
2023-12-15 00:09:07 +0800

    keypair generate algorithm config change

--- src/api/v1/services/auth.service.ts
+++ src/api/v1/services/auth.service.ts
@@ -153,16 +153,8 @@ export class AuthService {
     try {
       const { publicKey, privateKey } = generateKeyPairSync("rsa", {
         modulusLength: 2048,
-        publicKeyEncoding: {
-          type: "spki",
-          format: "pem"
-        },
-        privateKeyEncoding: {
-          type: "pkcs8",
-          format: "pem",
-          cipher: "aes-256-cbc",
-          passphrase: "top secret"
-        }
+        publicKeyEncoding: { type: "spki", format: "pem" },
+        privateKeyEncoding: { type: "pkcs8", format: "pem" }
       });
       // ene hesgiig queue-ruu hiine
       await vaultClient.write(`secret/data/${userId}`, {
